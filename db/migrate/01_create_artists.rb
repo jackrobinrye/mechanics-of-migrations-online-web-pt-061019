ca691a88a90c6d4bf1e5abb381f1f5dcd86c6135
Jack Atherton <jackrrye@gmail.com>
2019-08-28 18:06:44 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,4 +1,15 @@
-class CreateArtists < ActiveRecord::Migration[4.2]
+# db/migrate/01_create_artists.rb
+
+class CreateArtists < ActiveRecord::Migration[5.2]
+  def up
+  end
+
+  def change
+  end
+  
+  def down
+  end
+  
   def change
     create_table :artists do |t|
       t.string :name
@@ -7,4 +18,5 @@ class CreateArtists < ActiveRecord::Migration[4.2]
       t.string :hometown
     end
   end
+  
 end
